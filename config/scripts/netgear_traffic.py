67907fa75de196cde5940ac022ff19fd9f95975a
Jonas Friedmann <j@frd.mn>
2018-10-03 19:15:30 +0200

    Improve JSON dictionary

--- config/scripts/netgear_traffic.py
+++ config/scripts/netgear_traffic.py
@@ -6,4 +6,14 @@ from pynetgear import Netgear
 config = yaml.load(open('/config/secrets.yaml'))
 netgear = Netgear(host=config['netgear_host'], user=config['netgear_user'], password=config['netgear_pass'])
 
-print(json.dumps(netgear.get_traffic_meter()))
+traffic = netgear.get_traffic_meter()
+
+json_dict = {
+    "today": traffic['NewTodayUpload'] + traffic['NewTodayDownload'],
+    "yesterday": traffic['NewYesterdayUpload'] + traffic['NewYesterdayDownload'],
+    "week": traffic['NewWeekUpload'][0] + traffic['NewWeekDownload'][0],
+    "month": traffic['NewMonthUpload'][0] + traffic['NewMonthDownload'][0],
+    "lastmonth": traffic['NewLastMonthUpload'][0] + traffic['NewLastMonthDownload'][0],
+}
+
+print(json.dumps(json_dict))
