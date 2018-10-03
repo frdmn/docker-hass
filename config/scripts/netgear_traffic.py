deda8a4cd82079fbb7a06082636b4906d5cb7942
Jonas Friedmann <j@frd.mn>
2018-10-03 18:57:53 +0200

    Update relative path

--- config/scripts/netgear_traffic.py
+++ config/scripts/netgear_traffic.py
@@ -3,7 +3,7 @@ import yaml
 import json
 from pynetgear import Netgear
 
-config = yaml.load(open('../../secrets.yaml'))
+config = yaml.load(open('../secrets.yaml'))
 netgear = Netgear(host=config['netgear_host'], user=config['netgear_user'], password=config['netgear_pass'])
 
 print(json.dumps(netgear.get_traffic_meter()))
